7e2de7c10ad64299c9ca2948219134567eb85aec
chak1988 <88chak88@gmail.com>
2024-04-16 15:33:11 +0300

    modified

--- project-ec2-schedule/main.tf
+++ project-ec2-schedule/main.tf
@@ -1,10 +1,10 @@
 
-module "aws-ec2-schedule-log" {
-  source              = "../aws-ec2-module"
-  start_cron_schedule = "cron(00 07 ? * MON-FRI *)"
-  stop_cron_schedule  = "cron(00 17 ? * MON-FRI *)"
-  stopstart_tags      = { TagKEY : "log-collector-servers", TagVALUE : "enabled" }
-}
+# module "aws-ec2-schedule-log" {
+#   source              = "../aws-ec2-module"
+#   start_cron_schedule = "cron(00 07 ? * MON-FRI *)"
+#   stop_cron_schedule  = "cron(00 17 ? * MON-FRI *)"
+#   stopstart_tags      = { TagKEY : "log-collector-servers", TagVALUE : "enabled" }
+# }
 
 
 module "aws-ec2-schedule-cleaner" {
